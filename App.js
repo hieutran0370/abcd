b165840bb9bd77c9839306a19580076de38148d5
Admin <hieutph12400@fpt.edu.vn>
2021-04-22 21:13:04 +0700

    abcdef

--- App.js
+++ App.js
@@ -1,12 +1,23 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-
+import {NavigationContainer} from "@react-navigation/native";
+import {createStackNavigator} from "@react-navigation/stack";
+import List from "./abcef/List";
+import Sua from "./abcef/Sua";
+import Them from "./abcef/Them";
+import Xoa from "./abcef/Xoa";
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <NavigationContainer>
+        <Stack.Navigator initalRouteName="List">
+          <Stack.Screen name="List" options={{title: "Danh Sách Users"}} component={List}/>
+          <Stack.Screen name="Sua" options={{title: "Sửa"}} component={Sua}/>
+          <Stack.Screen name="Them" options={{title: "Thêm"}} component={Them}/>
+          <Stack.Screen name="Xoa" options={{title: "Xóa"}} component={Xoa}/>
+        </Stack.Navigator>
+      </NavigationContainer>
     </View>
   );
 }
